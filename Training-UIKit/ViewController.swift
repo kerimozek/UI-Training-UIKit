50482956df8e03d227f09b283eb6027a0d4a3b0a
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-03-17 21:51:08 +0300

    first commit

---
+++ Training-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training-UIKit
+//
+//  Created by Mehmet Kerim ÖZEK on 17.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
